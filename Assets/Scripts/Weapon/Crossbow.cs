3ac7919dddf213a578c10778ce8963f0aefa79ce
Mental <Alekov99@mail.ru>
2026-02-10 22:10:27 +0500

    Refactor weapon class

--- Assets/Scripts/Weapon/Crossbow.cs
+++ Assets/Scripts/Weapon/Crossbow.cs
@@ -1,38 +1,67 @@
+using System;
 using UnityEngine;
 
 public class Crossbow : Weapon
 {
-    [SerializeField] private WeaponConfig _config;
+    [Serializable]
+    private struct CrossbowModeConfig
+    {
+        [SerializeField, Tooltip("An amount of damage that enemies will take")] private float _damage;
+        [SerializeField, Tooltip("Reload time in seconds")] private float _reloadTime;
+        [SerializeField, Tooltip("Max shot distance")] private float _maxDistance;
+        [SerializeField, Tooltip("The sound that will be played when a shot is fired")] private AudioClip _firingSound;
+        [SerializeField, Tooltip("Layer mask for raycasts")] private LayerMask _layerMask;
+
+        public float Damage { get { return _damage; } }
+        public float ReloadTime { get { return _reloadTime; } }
+        public float MaxDistance { get { return _maxDistance; } }
+        public AudioClip FiringSound { get { return _firingSound; } }
+        public LayerMask LayerMask { get { return _layerMask; } }
+    }
+
+    [SerializeField] private CrossbowModeConfig _defaultMode;
+    [SerializeField] private CrossbowModeConfig _alternateMode;
     [SerializeField, Tooltip("An origin transform of a projectile")] private Transform _origin;
-    [SerializeField] private LayerMask _layerMask;
     [SerializeField] private AudioSource _audioSource;
 
     private float _lastShootTime = 0f;
 
     public override bool CanShoot()
     {
-        return Time.time - _lastShootTime > _config.ReloadTime;
+        return Time.time - _lastShootTime > _defaultMode.ReloadTime;
+    }
+
+    public override void CanShootAlternate()
+    {
+        throw new System.NotImplementedException();
     }
 
     public override void Shoot()
     {
-        _audioSource.PlayOneShot(_config.FiringSound);
+        _audioSource.PlayOneShot(_defaultMode.FiringSound);
 
-        bool isHit = Physics.Raycast(_origin.position, _origin.forward, out RaycastHit hitInfo, _config.MaxDistance, _layerMask);
+        bool isHit = Physics.Raycast(_origin.position, _origin.forward, out RaycastHit hitInfo, _defaultMode.MaxDistance, _defaultMode.LayerMask);
         if (isHit)
         {
             Debug.Log($"Hit object {hitInfo.collider.gameObject}");
-
-            if (hitInfo.collider.TryGetComponent(out Enemy enemy))
             {
-                enemy.TakeDamage(_config.Damage);
-                if (enemy.IsDead())
+
+                if (hitInfo.collider.TryGetComponent(out Enemy enemy))
                 {
-                    Debug.Log($"Killed an enemy ${hitInfo.collider.gameObject.ToString()}");
+                    enemy.TakeDamage(_defaultMode.Damage);
+                    if (enemy.IsDead())
+                    {
+                        Debug.Log($"Killed an enemy ${hitInfo.collider.gameObject.ToString()}");
+                    }
                 }
             }
-        }
 
-        _lastShootTime = Time.time;
+            _lastShootTime = Time.time;
+        }
+    }
+
+    public override void ShootAlternate()
+    {
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -5,4 +5,8 @@ public abstract class Weapon : MonoBehaviour
     public abstract void Shoot();
 
     public abstract bool CanShoot();
+
+    public abstract void ShootAlternate();
+
+    public abstract void CanShootAlternate();
 }
