d1e844a5baf3720bf579bf4f2f0ff84cb590d7b6
Mental <Alekov99@mail.ru>
2026-02-18 21:43:01 +0500

    Fix outline, melee controller

--- Assets/Scripts/Enemy/Limb.cs
+++ Assets/Scripts/Enemy/Limb.cs
@@ -3,17 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(Outline))]
 public class Limb : MonoBehaviour, IPickup
 {
-    Outline _outline;
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private Color _pickupColor;
+
+    private Outline _outline;
 
     public void Start()
     {
         _outline = GetComponent<Outline>();
-        _outline.enabled = false;
-    }
-
-    public void Outline(bool enabled)
-    {
-        _outline.enabled = enabled;
+        Outline(false);
     }
 
     public void PickUp(Player player)
@@ -21,5 +19,27 @@ public class Limb : MonoBehaviour, IPickup
         player.TakeNewMelee();
         Destroy(this);
         Destroy(gameObject, 0.1f);
+    }
+
+    public void Outline(bool enabled)
+    {
+        if (enabled)
+        {
+            SetPickupColor();
+        }
+        else
+        {
+            SetDefaultColor();
+        }
+    }
+
+    private void SetDefaultColor()
+    {
+        _outline.OutlineColor = _defaultColor;
+    }
+
+    private void SetPickupColor()
+    {
+        _outline.OutlineColor = _pickupColor;
     }
 }
--- Assets/Scripts/Player/MeleeController.cs
+++ Assets/Scripts/Player/MeleeController.cs
@@ -5,16 +5,16 @@ public class MeleeController : MonoBehaviour
     [SerializeField] private Transform _camera;
     [SerializeField] private float _damage = 100f;
     [SerializeField] private float _distance = 3f;
-    [SerializeField] private int _maxHits = 1;
+    [SerializeField] private int _maxEnemyHits = 1;
     [SerializeField] private LayerMask _hitLayer;
 
-    public int MaxHits { get { return _maxHits; } }
+    public int MaxHits { get { return _maxEnemyHits; } }
 
-    private int _currentHits = 0;
+    private int _currentEnemyHits = 0;
 
     public void Attack()
     {
-        if (_currentHits >= _maxHits)
+        if (_currentEnemyHits >= _maxEnemyHits)
         {
             return;
         }
@@ -24,20 +24,17 @@ public class MeleeController : MonoBehaviour
 
     public void SetCurrentHits(int hits)
     {
-        _currentHits = Mathf.Clamp(hits, 0, _maxHits);
+        _currentEnemyHits = Mathf.Clamp(hits, 0, _maxEnemyHits);
     }
 
     private void AttackRaycast()
     {
         bool isHit = Physics.Raycast(_camera.transform.position, _camera.transform.forward, out RaycastHit hitInfo, _distance, _hitLayer);
-        if (isHit)
+        if (isHit && hitInfo.collider.gameObject.TryGetComponent(out Enemy enemy))
         {
-            if (hitInfo.collider.gameObject.TryGetComponent(out Enemy enemy))
-            {
-                Debug.Log($"Hit enemy {enemy} with melee attack");
-                enemy.TakeDamage(_damage);
-            }
-            SetCurrentHits(_currentHits + 1);
+            Debug.Log($"Hit enemy {enemy} with melee attack");
+            enemy.TakeDamage(_damage);
+            SetCurrentHits(_currentEnemyHits + 1);
         }
     }
 }
