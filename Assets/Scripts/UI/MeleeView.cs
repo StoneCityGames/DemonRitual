83b1a9dc5e8b1d2420f59230d3a0ae6883e611ed
Mental <Alekov99@mail.ru>
2026-02-22 20:17:15 +0500

    Update UI

--- Assets/Scripts/UI/MeleeView.cs
+++ Assets/Scripts/UI/MeleeView.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 public class MeleeView : MonoBehaviour
 {
     [SerializeField] private MeleeController _meleeController;
-    [SerializeField] private Transform _meleeUI;
+    [SerializeField] private Transform _animatedUI;
+    [SerializeField] private UnityEngine.UI.Image _animatedImage;
+
     [SerializeField] private float _animationDuration = 2f;
     [SerializeField] private float _animationFallBackDuration = 1f;
+    [SerializeField] private float _animationFadeEndValue = 0.2f;
 
-    private Tweener _tween = null;
+    private Tweener _scaleTween = null;
+    private Tweener _colorTween = null;
+    private Color _initialAnimationColor;
+
+    private void Awake()
+    {
+        _initialAnimationColor = _animatedImage.color;
+    }
 
     private void OnEnable()
     {
@@ -34,22 +44,26 @@ public class MeleeView : MonoBehaviour
 
     private void StartAnimation()
     {
-        if (_tween != null && _tween.IsPlaying())
+        if (_scaleTween != null && _scaleTween.IsPlaying() || _colorTween != null && _colorTween.IsPlaying())
         {
             return;
         }
 
-        _tween = _meleeUI.DOScale(new Vector3(1.5f, 1.5f, 1.5f), _animationDuration).SetLoops(-1, LoopType.Yoyo);
+        _scaleTween = _animatedUI.DOScale(new Vector3(1.5f, 1.5f, 1.5f), _animationDuration).SetLoops(-1, LoopType.Yoyo);
+        _colorTween = _animatedImage.DOFade(_animationFadeEndValue, _animationDuration).SetLoops(-1, LoopType.Yoyo);
     }
 
     private void StopAnimation()
     {
-        if (_tween == null || _tween.IsComplete())
+        if (_scaleTween == null || _scaleTween.IsComplete())
         {
             return;
         }
 
-        _tween.Kill();
-        _meleeUI.DOScale(new Vector3(1f, 1f, 1f), _animationFallBackDuration);
+        _scaleTween.Kill();
+        _colorTween.Kill();
+
+        _animatedUI.DOScale(new Vector3(1f, 1f, 1f), _animationFallBackDuration);
+        _animatedImage.DOColor(_initialAnimationColor, _animationFallBackDuration);
     }
 }
